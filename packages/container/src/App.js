b7189243fa65a99e7a773cfb6d3e7cd2b881d4a6
soner.bedir <soner.bedir@hepsiburada.com>
2023-12-29 17:13:26 +0300

    Update text field

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
-        <h1>Hii There!!!</h1>
+        <h1>Hii There</h1>
         <hr/>
        <MarketingApp/>
     </div>
